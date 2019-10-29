17b67eef4ff231d320a565693b4b5e4954bf85b4
brynax0q <xq0x0q@163.com>
2019-10-29 16:33:30 +0800

    change

--- Node.java
+++ Node.java
@@ -44,22 +44,4 @@ public class Node {
         return start;
     }
 
-    public static void output(Node node) {
-        while (node != null) {
-            System.out.print(node.value);
-            System.out.print("->");
-            node = node.next;
-        }
-        System.out.println("null");
-    }
-
-    public static void main(String[] args) {
-
-        Node l1 = new Node(1);
-        l1.addToTail(2);
-        l1.addToTail(3);
-        l1.addToTail(4);
-        l1 = l1.removeNode(1);
-        output(l1);
-    }
 }
