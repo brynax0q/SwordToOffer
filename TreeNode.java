b05cb99a96ccbafc914e0418a1ac7fa31f5cd1d2
brynax0q <xq0x0q@163.com>
2019-10-30 20:08:23 +0800

    TreeNode

--- TreeNode.java
+++ TreeNode.java
@@ -1,9 +1,9 @@
 package swordtooffer;
-
+import java.util.Stack;
 public class TreeNode {
-    int value;
-    TreeNode left;
-    TreeNode right;
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
 
     public TreeNode(int value, TreeNode left, TreeNode right) {
         this.value = value;
@@ -15,5 +15,24 @@ public class TreeNode {
         this.value = value;
         this.left = null;
         this.right = null;
+    }
+
+    public void preOrder() {
+        if (this == null) {
+            return;
+        }
+        TreeNode p = this;
+        Stack<TreeNode> s = new Stack<>();
+        while (!s.isEmpty() || p != null) {
+            while (p != null) {
+                System.out.print(p.value + " ");
+                s.push(p);
+                p = p.left;
+            }
+            if (!s.isEmpty()) {
+                p = s.pop();
+                p = p.right;
+            }
+        }
     }
 }
