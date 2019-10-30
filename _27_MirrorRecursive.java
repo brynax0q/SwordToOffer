c781cc6a44e462c71654cd0c88255f8857ec4ae4
brynax0q <xq0x0q@163.com>
2019-10-30 20:30:00 +0800

    二叉树的镜像 递归&非递归

--- _27_MirrorRecursive.java
+++ _27_MirrorRecursive.java
@@ -1,9 +1,35 @@
 package swordtooffer;
 
+import java.util.Stack;
+
 /**
  * 交换左右子树
  */
 public class _27_MirrorRecursive {
+    public void mirrorNotRecursive(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode p = root;
+        while (true){
+            TreeNode tmp = p.left;
+            p.left = p.right;
+            p.right = tmp;
+            if (p.right != null) {
+                stack.push(p.right);
+            }
+            if (p.left != null) {
+                p = p.left;
+            }else{
+                if (stack.isEmpty()) {
+                    break;
+                }else{
+                    p = stack.pop();
+                }
+            }
+        }
+    }
     public void mirrorRecursive(TreeNode root) {
         if (root == null) {
             return;
@@ -11,7 +37,6 @@ public class _27_MirrorRecursive {
         if (root.left == null && root.right == null) {
             return;
         }
-
         TreeNode tmp = root.left;
         root.left = root.right;
         root.right = tmp;
