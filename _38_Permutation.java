99d461ba73a52d06d16ac313878c5261b6a93ae0
brynax0q <xq0x0q@163.com>
2019-11-06 19:00:59 +0800

    排列和组合

--- _38_Permutation.java
+++ _38_Permutation.java
@@ -3,7 +3,11 @@ package swordtooffer;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 字符串的排列与组合
+ */
 public class _38_Permutation {
+    // 排列
     public void permutation(char[] chs) {
         if (chs == null || chs.length == 0) {
             return;
@@ -30,8 +34,29 @@ public class _38_Permutation {
         }
     }
 
+    // 组合
+    public void combine(char[] chs) {
+        if (chs == null || chs.length == 0) {
+            return;
+        }
+        List<Character> tmp = new LinkedList<>();
+        combine_helper(chs, 0, tmp);
+    }
+
+    public void combine_helper(char[] chs, int index, List<Character> tmp) {
+        if (!tmp.isEmpty()) {
+            System.out.println(tmp);
+        }
+        for (int i = index; i < chs.length; i++) {
+
+            tmp.add(chs[i]);
+            combine_helper(chs, i + 1, tmp);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+
     public static void main(String[] args) {
         String s = "abc";
-        new _38_Permutation().permutation(s.toCharArray());
+        new _38_Permutation().combine(s.toCharArray());
     }
 }
