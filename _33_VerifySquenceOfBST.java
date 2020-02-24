45c835e58649627c7a1941be92c73b991cc942b3
brynax0q <xq0x0q@163.com>
2020-02-24 21:44:56 +0800

    fix bug

--- _33_VerifySquenceOfBST.java
+++ _33_VerifySquenceOfBST.java
@@ -1,45 +1,35 @@
 package swordtooffer;
 
+import algorithm.二分搜索._33;
+
 /**
- * 二叉搜索树的后序遍历
+ * 二叉搜索树的后序遍历序列
  * intput：输入一个整数数组
  * output：判断该数组是不是某个二叉搜索树的后续遍历结果
  */
 public class _33_VerifySquenceOfBST {
-    public boolean verifySquenceOfBST(int[] sequence, int l, int r) {
-        if (sequence == null || r <= l) {
+
+    public boolean verifyPostorder(int[] sequence) {
+        if (sequence == null || sequence.length == 0) {
+            return true;
+        }
+        return verifySquenceOfBST(sequence, 0, sequence.length - 1);
+    }
+    public boolean verifySquenceOfBST(int[] sequence, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        int i = start;
+        int j = end-1;
+        while (i < end && sequence[i] < sequence[end]) {
+            i++;
+        }
+        while (j >= start && sequence[j] > sequence[end]) {
+            j--;
+        }
+        if (i < j) {
             return false;
         }
-        int root = sequence[r - 1];
-        int i = l;
-
-        // 搜索左子树节点的值小于根节点的值
-        for (; i < r - 1; i++) {
-            if (sequence[i] > root) {
-                break;
-            }
-        }
-
-        // 搜索右子树的值大于根节点的值
-        int j = i;
-        for (; j < r - 1; j++) {
-            if (sequence[j] < root) {
-                return false;
-            }
-        }
-
-        // 判断左子树
-        boolean left = true;
-        if (i > 0) {
-            left = verifySquenceOfBST(sequence, 0, i);
-        }
-
-        // 判断右子树
-        boolean right = true;
-        if (i < j) {
-            right = verifySquenceOfBST(sequence, i, j);
-        }
-        return left && right;
+        return verifySquenceOfBST(sequence, start, i - 1) && verifySquenceOfBST(sequence, j + 1, end - 1);
     }
-
 }
